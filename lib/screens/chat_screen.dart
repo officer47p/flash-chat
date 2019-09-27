16b32fd89ce560f5fa1b0d98828a090f7fe302dd
Parsa Hosseini <officer47p@gmail.com>
2019-09-27 21:50:02 +0330

    added authentication and many cool animation in the auth part

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatefulWidget {
   static String id = "chat";
@@ -9,6 +10,28 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    await Future.delayed(Duration(seconds: 3), () {});
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+        print("from chat ${user.email}");
+      }
+    } catch (err) {
+      print(err);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static String id = "registration";
@@ -8,54 +12,150 @@ class RegistrationScreen extends StatefulWidget {
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen> {
+class _RegistrationScreenState extends State<RegistrationScreen>
+    with TickerProviderStateMixin {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+  bool showLoading = false;
+  bool hasSubmittedForm = false;
+  AnimationController controller;
+
+  AnimationController createController() {
+    var cont = AnimationController(
+        duration: Duration(milliseconds: 200), vsync: this, upperBound: 0.9);
+    cont.addListener(() {
+      setState(() {});
+    });
+    return cont;
+  }
+
+  void getCurrentUser() async {
+    print("from registration ${(await _auth.currentUser()).email}");
+  }
+
   @override
   Widget build(BuildContext context) {
+    getCurrentUser();
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: "logo",
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
-              ),
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Hero(
+                  tag: "logo",
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                SizedBox(
+                  height: 48.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    email = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: "Enter your email",
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  textAlign: TextAlign.center,
+                  obscureText: true,
+                  onChanged: (value) {
+                    password = value;
+                  },
+                  decoration: kTextFieldDecoration.copyWith(
+                    hintText: "Enter your password",
+                  ),
+                ),
+                SizedBox(
+                  height: 24.0,
+                ),
+                RoundedButton(
+                  color: Colors.blueAccent,
+                  text: "Register",
+                  onPressed: () async {
+                    if (!hasSubmittedForm) {
+                      hasSubmittedForm = true;
+                      try {
+                        setState(() {
+                          controller = createController();
+                          controller.forward();
+                          showLoading = true;
+                        });
+                        AuthResult authResult =
+                            await _auth.createUserWithEmailAndPassword(
+                                email: email, password: password);
+                        await Navigator.pushNamed(context, ChatScreen.id);
+                        setState(() {
+                          hasSubmittedForm = false;
+                          showLoading = false;
+                          controller.dispose();
+                          controller = null;
+                        });
+                      } catch (err) {
+                        print("error");
+                        print(err);
+                        setState(() {
+                          hasSubmittedForm = false;
+                          showLoading = false;
+                          controller.dispose();
+                          controller = null;
+                        });
+                      }
+                    }
+                  },
+                ),
+              ],
             ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: "Enter your email"),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: "Enter your password"),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.blueAccent,
-              text: "Register",
-              onPressed: () {},
-            ),
-          ],
-        ),
+          ),
+          showLoading
+              ? Container(
+                  color: Colors.black.withOpacity(controller.value),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SpinKitDoubleBounce(
+                        color: Colors.red,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            "Registering User",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          TypewriterAnimatedTextKit(
+                            text: ['...'],
+                            duration: Duration(milliseconds: 3000),
+                            textStyle: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              : SizedBox(),
+        ],
       ),
     );
   }
